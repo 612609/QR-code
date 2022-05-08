59a6b1f286ed8367f00d688a32b0e7271670b934
612609 <104626269+612609@users.noreply.github.com>
2022-05-08 17:58:41 +0700

    init 3

--- app/src/main/java/com/example/myapplication/activity_generator.java
+++ app/src/main/java/com/example/myapplication/activity_generator.java
@@ -1,14 +1,21 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.SharedValues;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
@@ -16,11 +23,19 @@ import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class activity_generator extends AppCompatActivity implements View.OnClickListener {
     private EditText edit_code;
     Button generator;
     private Button save;
     private ImageView qrCode;
+    Button SaveCode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +43,16 @@ public class activity_generator extends AppCompatActivity implements View.OnClic
         generator=(Button) findViewById(R.id.generator);
         generator.setOnClickListener(this);
         init();
+        qrCode=(ImageView) findViewById(R.id.qr_code);
+        SaveCode=(Button) findViewById(R.id.SaveCode);
+        ActivityCompat.requestPermissions(activity_generator.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        ActivityCompat.requestPermissions(activity_generator.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        SaveCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SaveToGallery();
+            }
+        });
     }
 
     @Override
@@ -41,6 +66,8 @@ public class activity_generator extends AppCompatActivity implements View.OnClic
                 break;
         }
     }
+
+
     private void init(){
         edit_code=findViewById(R.id.edit_code);
         qrCode=findViewById(R.id.qr_code);
@@ -67,5 +94,31 @@ public class activity_generator extends AppCompatActivity implements View.OnClic
         BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
         Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
         qrCode.setImageBitmap(bitmap);
+    }
+    private void SaveToGallery(){
+        BitmapDrawable bitmapDrawable=(BitmapDrawable) qrCode.getDrawable();
+        Bitmap bitmap= bitmapDrawable.getBitmap();
+        FileOutputStream outputStream= null;
+        File file =Environment.getExternalStorageDirectory();
+        File dir=new File(file.getAbsolutePath()+"/MyPics");
+        dir.mkdirs();
+        String filename = String.format("%d.png",System.currentTimeMillis());
+        File outFile=new File(dir,filename);
+        try {
+            outputStream=new FileOutputStream(outFile);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+        try {
+            outputStream.flush();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        try {
+            outputStream.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/scannerView.java
+++ app/src/main/java/com/example/myapplication/scannerView.java
@@ -2,7 +2,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.os.Bundle;
-import android.widget.TextView;
+
 import com.google.zxing.Result;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
